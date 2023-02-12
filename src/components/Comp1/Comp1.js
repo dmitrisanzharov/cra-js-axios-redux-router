112a3b5e21cf4e6aab282a2ebf2e53cbca49fe27
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-02-12 18:19:14 +0000

    good

--- src/components/Comp1/Comp1.js
+++ src/components/Comp1/Comp1.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Comp1.css";
+import { useForm } from "react-hook-form";
 
 const Comp1 = () => {
-	return <div className="Comp1">Comp1</div>;
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log("submit success", data);
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			{/* register your input into the hook by invoking the "register" function */}
+			<select {...register("name", { required: true, pattern: /^(?!select)/ })}>
+				<option value="select">please select</option>
+				<option value="name1">name</option>
+				<option value="name2">name2</option>
+			</select>
+
+			{errors?.name?.type === "required" && <span>required</span>}
+			{errors?.name?.type === "pattern" && <span>pattern</span>}
+
+			<input type="submit" />
+		</form>
+	);
 };
 
 export default Comp1;
