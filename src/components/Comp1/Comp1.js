9e6c2e6d5753af0434dbfa323dd17405e344a0d9
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-02-24 12:15:19 +0000

    commit

--- src/components/Comp1/Comp1.js
+++ src/components/Comp1/Comp1.js
@@ -1,8 +1,88 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import "./Comp1.css";
+import data from "./data";
+import classNames from "classnames";
 
 const Comp1 = () => {
-	return <div className="Comp1">Comp1</div>;
+	const [idxOfVisible, setIdxOfVisible] = useState(0);
+	const [direction, setDirection] = useState("");
+	const [disableBtn, setDisableBtn] = useState(false);
+
+	useEffect(() => {
+		console.log("idxOfVisible", idxOfVisible);
+
+		if (idxOfVisible === 5) {
+			const timeOut = setTimeout(() => {
+				setIdxOfVisible(1);
+				setDisableBtn(false);
+			}, 501);
+			return () => clearTimeout(timeOut);
+		}
+
+		if (idxOfVisible === 0) {
+			const timeOut = setTimeout(() => {
+				setIdxOfVisible(4);
+				setDisableBtn(false);
+			}, 501);
+			return () => clearTimeout(timeOut);
+		}
+
+		const timeOut = setTimeout(() => {
+			setDisableBtn(false);
+		}, 501);
+		return () => clearTimeout(timeOut);
+	}, [idxOfVisible]);
+
+	return (
+		<div className="mainContainer drr">
+			<div>
+				<button
+					onClick={() => {
+						setIdxOfVisible(idxOfVisible - 1);
+						setDirection("left");
+						setDisableBtn(true);
+					}}
+					disabled={disableBtn}
+				>
+					left
+				</button>
+				<button
+					onClick={() => {
+						setIdxOfVisible(idxOfVisible + 1);
+						setDirection("right");
+						setDisableBtn(true);
+					}}
+					disabled={disableBtn}
+				>
+					right
+				</button>
+				<hr />
+			</div>
+
+			<div className="frame dbb">
+				<div
+					className={classNames("sliderBox dyy", {
+						// classname : condition
+						"noAnimation--yes":
+							(idxOfVisible === 4 && direction === "left") ||
+							(idxOfVisible === 1 && direction === "right"),
+					})}
+					style={{ transform: `translateX(${idxOfVisible * -292}px)` }}
+				>
+					{data?.map((el, i) => {
+						const { name, image } = el;
+						return (
+							<div
+								className="singleItem"
+								key={name + i}
+								style={{ backgroundImage: `url(${image})` }}
+							></div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Comp1;
--- src/components/Comp2/Comp2.js
+++ src/components/Comp2/Comp2.js
@@ -2,7 +2,18 @@ import React, { useEffect, useState, useRef, useMemo } from "react";
 import "./Comp2.css";
 
 const Comp2 = () => {
-	return <div className="Comp2">Comp2</div>;
+	return (
+		<div className="container drr">
+			<div className="flex-container dbb">
+				<div className="item">First item</div>
+				<div className="item">Second item</div>
+				<div className="item">Third item</div>
+				<div className="item">Fourth item</div>
+				<div className="item">Fifth item</div>
+				<div className="item">Sixth item</div>
+			</div>
+		</div>
+	);
 };
 
 export default Comp2;
