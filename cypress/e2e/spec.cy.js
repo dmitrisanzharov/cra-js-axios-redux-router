206953f80e934c87851cd201926c689736b8cd7a
Victor Sarov <victor.sarov@rentalmatics.com>
2023-07-04 11:20:11 +0100

    Cypress passes all good

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,15 @@ describe('template spec', () => {
     cy.visit('http://web.local.rentalmatics.com/');
     cy.get('input[name="email"]').type('victor.sarov@rentalmatics.com');
     cy.get('input[name="password"]').type('qwert12345!');
-    cy.get('button[type="submit"]').click()
+    cy.get('button[type="submit"]').click();
+    cy.visit('http://localhost:3000/');
+    cy.origin('http://localhost:3000/', () => {
+      cy.get('#vid').invoke('css', 'z-index').then(el => 
+          expect(el).to.eq('2')
+      )
+      cy.get('#vid-td').invoke('css', 'z-index').then(el =>
+        expect(el).to.eq('1')
+      );
+    })
   })
 })
--- src/components/Comp1/Comp1.js
+++ src/components/Comp1/Comp1.js
@@ -3,7 +3,26 @@ import "./Comp1.css";
 import { useForm } from "react-hook-form";
 
 const Comp1 = () => {
-	return <div className="Comp1">Comp1</div>;
+	return <div className="Comp1">
+		<h1>Rentalmatics Table</h1>
+		<table>
+			<tr>
+				<th>Car</th>
+				<th id='vid'>Registration, zI: 2</th>
+				<th>Milage</th>
+			</tr>
+			<tr>
+				<td>BMW</td>
+				<td id='vid-td'>21-D-9087, zI: 1</td>
+				<td>100,000</td>
+			</tr>
+			<tr>
+				<td>Audi</td>
+				<td>19-D-2345</td>
+				<td>90,000</td>
+			</tr>
+		</table>
+	</div>;
 };
 
 export default Comp1;
