f586d1f349a8cf5b319cf5fe4538200272132e53
Victor Sarov <victor.sarov@rentalmatics.com>
2023-07-12 13:28:10 +0100

    all good

--- src/components/Comp1/Comp1.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import "./Comp1.css";
-import { useForm } from "react-hook-form";
-
-const Comp1 = () => {
-	return <div className="Comp1">Comp1</div>;
-};
-
-export default Comp1;
--- src/components/Comp2/Comp2.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import "./Comp2.css";
-
-const Comp2 = () => {
-	return <div className="Comp2">Comp2</div>;
-};
-
-export default Comp2;
