b621e4775665de04dd1d6bbb02747eb520ab0efd
Victor Sarov <victor.sarov@rentalmatics.com>
2023-07-14 19:52:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 
 // components
-import Comp1 from "./components/Comp1/Comp1";
-import Comp2 from "./components/Comp2/Comp2";
+
 
 function App() {
 	return (
 		<div className="addPadding">
-			<Comp1 />
-			{/* <Comp2 /> */}
+
 		</div>
 	);
 }
