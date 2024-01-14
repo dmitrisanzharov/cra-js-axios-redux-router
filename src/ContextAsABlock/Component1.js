bf57b5b815a7a347d30d2784453e3e65d82d0ddb
fruitloaf <dmitrisanzharov@gmail.com>
2024-01-14 19:42:26 +0000

    comments

--- src/ContextAsABlock/Component1.js
+++ src/ContextAsABlock/Component1.js
@@ -3,7 +3,7 @@ import { MyContext } from './ContextAsABlockWrapper'
 
 const Component1 = () => {
 
-  const objValues= React.useContext(MyContext);
+  const objValues= React.useContext(MyContext); // this can be named as ANYTHING i want... objValues OR shakeAndBake or contextData, etc.
   objValues.comp1 = 'comp1'
   console.log("Component 1: objValues: ", objValues);
 
